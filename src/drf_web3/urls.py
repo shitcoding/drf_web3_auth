0c90ff08805c607716bcfac3784b82984aaf92cd
Vasiliy Ivanov <shitcoder@pm.me>
2023-12-26 16:51:49 +0300

    Add url routes for getting nonce and web3 auth

--- src/drf_web3/urls.py
+++ src/drf_web3/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import EventViewSet
+from .views import EthAccountNonceView, EventViewSet, Web3AuthView
 
 router = DefaultRouter()
 router.register('events', EventViewSet, basename='events')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('nonce/<str:address>/', EthAccountNonceView.as_view(), name='nonce'),
+    path('auth/web3/', Web3AuthView.as_view(), name='web3-auth'),
 ]
