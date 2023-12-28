59b9a0cd64e06929c8205432dcd1aeedc4df55a9
Vasiliy Ivanov <shitcoder@pm.me>
2023-12-28 17:50:25 +0300

    Change url routes

--- src/drf_web3/urls.py
+++ src/drf_web3/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import EthAccountMessageView, EventViewSet, Web3AuthView
+from .views import EventViewSet, MessageView, Web3AuthView
 
 router = DefaultRouter()
 router.register('events', EventViewSet, basename='events')
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path(
         'message/<str:eth_address>/',
-        EthAccountMessageView.as_view(),
+        MessageView.as_view(),
         name='message',
     ),
     path('auth/web3/', Web3AuthView.as_view(), name='web3-auth'),
